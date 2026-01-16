8a2e560083f0b512801e1bf819650a9e3add9935
思博 <siboxu3@gmail.com>
2026-01-17 00:06:04 +0800

    happy test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::env;
+// mtkcli -i input.csv
+
+struct Args {
+    pwd: String,
+    input: String,
+}
+
+impl Args {
+    fn new() -> Args {
+        Args { pwd: String::new(), input: String::new() }
+    }
+}
+
 fn main() {
-    mtk-cli::run();
+    let env_args_collection: Vec<String> = env::args().collect();
+    //获取pwd
+    let mut args = Args::new();
+    args.pwd = env_args_collection[0].clone();
+
+    let mut index = 1;
+    while index < env_args_collection.len() {
+        if env_args_collection[index] == "-i" && env_args_collection[index + 1] != "" {
+            args.input = env_args_collection[index + 1].clone();
+            break;
+        }
+        index += 1;
+    }
+    println!("pwd: {}, input: {}", args.pwd, args.input);
 }
